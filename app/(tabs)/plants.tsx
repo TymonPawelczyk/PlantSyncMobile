533981c82ecd19a6f3c41949a76d04f22bc00d80
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-10 02:30:17 +0200

    Refactor PlantsScreen to use SafeAreaView and ScrollView for improved layout and safety on different devices

--- app/(tabs)/plants.tsx
+++ app/(tabs)/plants.tsx
@@ -1,21 +1,35 @@
-import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, ScrollView,} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 export default function PlantsScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Plants screen</Text>
-    </View>
+    <SafeAreaProvider>
+    <SafeAreaView style={styles.container} edges={['top']}>
+      <StatusBar style="auto" backgroundColor="#000" />    
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+          Plants screen
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  </SafeAreaProvider>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  scrollView: {
+    backgroundColor: '#25292e',
   },
   text: {
     color: '#fff',
-  }
+    fontSize: 42,
+    padding: 12,
+  },
   
 });
