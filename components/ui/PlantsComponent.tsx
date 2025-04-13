eed83b7cff1108069d914d17a5850ba3f9105e68
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-14 01:25:19 +0200

    Wrap ThemedText in Pressable for improved interactivity in PlantComponent

--- components/ui/PlantsComponent.tsx
+++ components/ui/PlantsComponent.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {View,  StyleSheet } from 'react-native';
+import {View,  StyleSheet, Pressable } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 
 
-export default function PlantComponent()  {
+export default function PlantComponent() {
   return (
       <View>
-        <ThemedText style={styles.text} >Plant item</ThemedText>
+        <Pressable>
+          <ThemedText style={styles.text} >Plant item</ThemedText>
+        </Pressable>
       </View>
     );
   }
