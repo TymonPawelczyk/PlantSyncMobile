b7d96d3414db3aa4d97211e3e4f41860192f7fdf
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-14 21:47:17 +0200

    Refactor PlantComponent to apply styles to the View for improved layout and presentation

--- components/ui/PlantsComponent.tsx
+++ components/ui/PlantsComponent.tsx
@@ -6,7 +6,7 @@ import { ThemedText } from '@/components/ThemedText';
 
 export default function PlantComponent() {
   return (
-      <View>
+      <View style={styles.view}>
         <Pressable>
           <ThemedText style={styles.text} >Plant item</ThemedText>
         </Pressable>
@@ -16,7 +16,17 @@ export default function PlantComponent() {
 
 
 const styles = StyleSheet.create({
+  view:{
+    padding: 12,
+    marginVertical: 6,
+    marginHorizontal: 14,
+    borderColor: '#fff',
+    borderBottomWidth: 0.2,
+    backgroundColor: '#f8f9fa',
+    borderRadius: 8,
+  },
   text: {
+    color: '#343a40',
     fontSize: 12,
   }
 });
