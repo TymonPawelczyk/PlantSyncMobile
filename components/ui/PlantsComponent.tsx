6ed4518896765f9b15a0fe5eadd8b701f635f728
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-14 22:12:51 +0200

    Refactor PlantComponent to improve structure, enhance styling, and update props for better flexibility

--- components/ui/PlantsComponent.tsx
+++ components/ui/PlantsComponent.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import {View,  StyleSheet, Pressable } from 'react-native';
+import { View, Pressable } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 import { ThemedText } from '@/components/ThemedText';
 
+// Define props, extending ScrollViewProps
+export type ThemedPlantComponentProps =  {
+  style?: ViewStyle;
+  contentContainerStyle?: ViewStyle;
+};  
 
-export default function PlantComponent() {
+export default function PlantComponent(props: ThemedPlantComponentProps) {
+    const colorScheme = useColorScheme(); // Get system color scheme
+    const colors = {
+      background: colorScheme === 'dark' ? '#343a40' : '#edede9',
+      text: colorScheme === 'dark' ? '#D8D7D4' : '#212529',
+    };
+    const { style, contentContainerStyle, ...otherProps } = props;
+
   return (
-      <View style={styles.view}>
+      <View style={[styles.container, { backgroundColor: colors.background }, style]}{...otherProps}>
         <Pressable>
-          <ThemedText style={styles.text} >Plant item</ThemedText>
+          <ThemedText style={styles.title}>Arbuz</ThemedText>
+          <ThemedText style={styles.description}>Description</ThemedText>
+          <ThemedText style={styles.edit}>Edit item</ThemedText>
         </Pressable>
       </View>
     );
@@ -16,17 +33,23 @@ export default function PlantComponent() {
 
 
 const styles = StyleSheet.create({
-  view:{
+  container:{
+    height: 120,
     padding: 12,
     marginVertical: 6,
     marginHorizontal: 14,
-    borderColor: '#fff',
-    borderBottomWidth: 0.2,
-    backgroundColor: '#f8f9fa',
     borderRadius: 8,
   },
-  text: {
-    color: '#343a40',
+  title: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: '300',
+  },
+  edit: {
     fontSize: 12,
-  }
+    fontWeight: '300',
+  },
 });
