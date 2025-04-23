070ad0f7efd5d7e2a728789eaf42f26a76634468
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-23 23:57:14 +0200

    Update EditPlantComponent to display pencil icon instead of text

--- components/ui/EditPlantComponent.tsx
+++ components/ui/EditPlantComponent.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+// import { Ionicons } from "@expo/vector-icons";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function EditPlantComponent() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#D8D7D4", fontSize: 24 }}>Edit</Text>
+      <Text style={{ color: "#D8D7D4", fontSize: 24 }}>
+        <Ionicons name="pencil" size={24} color="#D8D7D4" />
+      </Text>
     </View>
   );
 }
