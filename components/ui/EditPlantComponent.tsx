89a08164dd7062ac0318e725e0529819d6523204
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-23 23:58:05 +0200

    Remove commented import statement for Ionicons in EditPlantComponent

--- components/ui/EditPlantComponent.tsx
+++ components/ui/EditPlantComponent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-// import { Ionicons } from "@expo/vector-icons";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function EditPlantComponent() {
