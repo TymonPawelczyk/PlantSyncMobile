31c31fa5a553790de4082a87a01bab513ed82e7c
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-24 23:09:02 +0200

    Fix import statement for Ionicons in PlantsComponent

--- components/ui/PlantsComponent.tsx
+++ components/ui/PlantsComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, ViewStyle, Pressable, View, Image } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { ThemedText } from "@/components/ThemedText";
 
