7a5529e48a983a89ba4f4d352b4165e0cffa2f10
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-08-16 00:29:06 +0200

    refactor: simplify useColorScheme by utilizing useTheme for context access

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,6 +1,6 @@
-import { useContext } from "react";
-import { ThemeContext } from "./ThemeProvider";
+import { useTheme } from './ThemeProvider';
 
 export function useColorScheme() {
-  return useContext(ThemeContext).theme;
+  const { theme } = useTheme();
+  return theme;
 }
